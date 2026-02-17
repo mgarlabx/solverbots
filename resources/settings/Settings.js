94c216aeae06d761155b816cc063070f2cf5cc5d
Mauricio Garcia <mauricio@solverank.tech>
2026-02-17 16:26:52 -0300

    .

--- resources/settings/Settings.js
+++ resources/settings/Settings.js
@@ -1,9 +1,11 @@
 const localHosts = ['localhost', '127.0.0.1'];
 const isLocalEnvironment = localHosts.includes(window.location.hostname);
 
+const productionBackendPath = `${window.location.protocol}//${window.location.hostname}/solverbots/backend/`;
+
 const defaultBackendPath = isLocalEnvironment
     ? 'http://localhost:8888/_solveredu/solverbots/backend/'
-    : 'https://solvertank.tech/solverbots/backend/';
+    : productionBackendPath;
 
 const Settings = {
     backendPath: window.SOLVERBOTS_BACKEND_PATH || defaultBackendPath
